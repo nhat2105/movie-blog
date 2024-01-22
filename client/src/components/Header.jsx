6b9ae6d91850e0259eb64958b55ec000bcbb8f13
nhat2105 <nhattruong210503@gmail.com>
2024-01-21 17:02:20 -0700

    Finished Header design

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 import logo from '../assets/images/components/logo.jpg'
@@ -16,7 +16,7 @@ const mainNav = [
     },
     
     {
-        display: 'About us',
+        display: 'About',
         path: '/about'
     }
 ]
@@ -27,30 +27,19 @@ const Header = () => {
       <div className="container">
         <div className="header_logo">
             <Link to ='/'>
-                <img src={logo} alt= "" width= "10%"/>
+                <img className = "logo_img" src={logo} alt= ""/>
             </Link>
         </div>
         <div className="header_menu">
-            <div className="header_menu_left">
-                <div className="header__menu__left__close">
-                    <i className='bx bx-chevron-left'></i>
-                </div>
-                {
-                    mainNav.map((item, index) => (
-                        <div className="header_menu_left_item">
-                            <Link to={item.path}>
-                                <span>{item.display}</span>
-                            </Link>
-                        </div>
-                    ))
-                }
-            </div>
-
-            <div className="header_menu_right">
-                <div className="header__menu__item">
-                    <i className="bx bx-search"></i>
-                </div>
-            </div>
+            {
+                mainNav.map((item, index) => (
+                    <div className="header_menu_item" key = {index}>
+                        <Link to={item.path}>
+                            <span>{item.display}</span>
+                        </Link>
+                    </div>
+                ))
+            }
         </div>
       </div>
     </div>
